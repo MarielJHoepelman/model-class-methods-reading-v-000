dd6c3cbc220817606bae7f078524b4963d8859f2
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-03-03 15:57:36 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
 
 def index
  @authors = Author.all
-
    if !params[:author].blank?
      @posts = Post.by_author(params[:author])
    elsif !params[:date].blank?
